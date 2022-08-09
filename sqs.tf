351891f9ea2985ecb123168e4f0a969866898e8c
sivakrishnako <101097114+sivakrishnako@users.noreply.github.com>
2022-08-09 15:34:03 +0530

    Update sqs.tf

--- sqs.tf
+++ sqs.tf
@@ -1,16 +1,16 @@
-resource "aws_sqs_queue" "this" {
-  name                      = "${lower(local.local_data.tag_prefix)}-dailybatchdata-sqs-${lower(local.local_data.tag_env)}-${lower(local.local_data.tag_project)}"
-  delay_seconds             = 90
-  max_message_size          = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
-  tags = {
-    Name                    = "${lower(local.local_data.tag_prefix)}-dailybatchdata-sqs-${lower(local.local_data.tag_env)}-${lower(local.local_data.tag_project)}"
-  }
-}
+# resource "aws_sqs_queue" "this" {
+#   name                      = "${lower(local.local_data.tag_prefix)}-dailybatchdata-sqs-${lower(local.local_data.tag_env)}-${lower(local.local_data.tag_project)}"
+#   delay_seconds             = 90
+#   max_message_size          = 2048
+#   message_retention_seconds = 86400
+#   receive_wait_time_seconds = 10
+#   tags = {
+#     Name                    = "${lower(local.local_data.tag_prefix)}-dailybatchdata-sqs-${lower(local.local_data.tag_env)}-${lower(local.local_data.tag_project)}"
+#   }
+# }
 
 
-resource "aws_lambda_event_source_mapping" "kiosk-sqs-lambdamapping"{
-   event_source_arn = aws_sqs_queue.this.arn 
-   function_name = aws_lambda_function.kiosk-lambda.arn 
-   }
+# resource "aws_lambda_event_source_mapping" "kiosk-sqs-lambdamapping"{
+#    event_source_arn = aws_sqs_queue.this.arn 
+#    function_name = aws_lambda_function.kiosk-lambda.arn 
+#    }
