43c81fba433b879a47f5097da43282d3bc2d7ab5
sivakrishnako <101097114+sivakrishnako@users.noreply.github.com>
2022-08-09 13:39:23 +0530

    Update sqs.tf

--- sqs.tf
+++ sqs.tf
@@ -8,3 +8,9 @@ resource "aws_sqs_queue" "this" {
     Name                    = "${lower(local.local_data.tag_prefix)}-dailybatchdata-sqs-${lower(local.local_data.tag_env)}-${lower(local.local_data.tag_project)}"
   }
 }
+
+
+resource "aws_lambda_event_source_mapping" "kiosk-sqs-lambdamapping"{
+   event_source_arn = aws_sqs_queue.this.arn 
+   function_name = aws_lambda_function.kiosk-lambda.arn 
+   }
