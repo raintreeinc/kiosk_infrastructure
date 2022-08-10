b92ff60d6cb913e98de9826facec9c415ccf5519
sivakrishnako <101097114+sivakrishnako@users.noreply.github.com>
2022-08-10 18:41:32 +0530

    Create s3insuranc.tf

---
+++ s3insuranc.tf
@@ -0,0 +1,25 @@
+ resource "aws_s3_bucket" "event" {
+  bucket        = "${lower(local.local_data.tag_prefix)}-s3-insurancedata-${lower(local.local_data.tag_env)}-${lower(local.local_data.tag_project)}"
+  tags = {
+    Name        = "${lower(local.local_data.tag_prefix)}-s3-insurancedata-${lower(local.local_data.tag_env)}-${lower(local.local_data.tag_project)}"
+  }
+  lifecycle {
+    ignore_changes = [
+      server_side_encryption_configuration
+    ]
+  }
+}
+
+resource "aws_s3_bucket_acl" "event" {
+  bucket                = aws_s3_bucket.event.id
+  acl                   = "private"
+}
+
+
+
+resource "aws_s3_bucket_versioning" "event" {
+  bucket = aws_s3_bucket.event.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
