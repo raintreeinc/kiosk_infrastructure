5a789aea84d663ea43eb4a791f8a9da6bfc6a0d0
Sushant Sonarghare <sushants@cybage.com>
2022-08-17 00:16:55 +0530

    Update custom_domain.tf

--- custom_domain.tf
+++ custom_domain.tf
@@ -1,5 +1,5 @@
 resource "aws_api_gateway_domain_name" "adminapigateway" {
-  domain_name              = "rt-adminapi-dev-kiosk.dev.raintreeinc.com"
+  domain_name              = "rt-adminapi-kiosk.dev.raintreeinc.com"
   regional_certificate_arn = "arn:aws:acm:us-east-1:106367354196:certificate/809e311a-024e-4c98-bc72-3ae368a577af"
   endpoint_configuration {
     types = ["REGIONAL"]
@@ -7,7 +7,7 @@ resource "aws_api_gateway_domain_name" "adminapigateway" {
 }
 
 resource "aws_api_gateway_domain_name" "kioskapigateway" {
-  domain_name              = "rt-api-dev-kiosk.dev.raintreeinc.com"
+  domain_name              = "rt-api-kiosk.dev.raintreeinc.com"
   regional_certificate_arn = "arn:aws:acm:us-east-1:106367354196:certificate/809e311a-024e-4c98-bc72-3ae368a577af"
   endpoint_configuration {
     types = ["REGIONAL"]
