483997ee84dcb3b6e46e4c22c65d38bb2b9d8c9e
sivakrishnako <101097114+sivakrishnako@users.noreply.github.com>
2022-08-16 14:36:10 +0530

    Update custom_domain.tf

--- custom_domain.tf
+++ custom_domain.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_domain_name" "example" {
   domain_name              = "rt-adminapi-dev-kiosk.dev.raintreeinc.com"
-  regional_certificate_arn = "certificate arn"
+  regional_certificate_arn = "arn:aws:acm:us-east-1:106367354196:certificate/809e311a-024e-4c98-bc72-3ae368a577af"
   endpoint_configuration {
     types = ["REGIONAL"]
   }
