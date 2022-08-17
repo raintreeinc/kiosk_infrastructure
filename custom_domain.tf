e2fdf4e9628303358da8bf16fb85c28c2ba13c05
sivakrishnako <101097114+sivakrishnako@users.noreply.github.com>
2022-08-17 17:04:16 +0530

    Update custom_domain.tf

--- custom_domain.tf
+++ custom_domain.tf
@@ -21,7 +21,7 @@ resource "aws_api_gateway_domain_name" "kioskapigateway" {
 
 
 resource "aws_api_gateway_base_path_mapping" "backend" {
-  api_id      = "rt-s3-lambda-webapp-batch-sqa-kiosk"
+  api_id      = aws_api_gateway_rest_api.rt-s3-lambda-webapp-batch-sqa-kiosk.id
   domain_name = aws_api_gateway_domain_name.adminapigateway.id
   stage_name = "dev"
 }
