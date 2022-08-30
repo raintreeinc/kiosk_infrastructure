6652ef437112897624f636d446ef0111ae4cda10
Sushant Sonarghare <sushants@cybage.com>
2022-08-30 12:01:57 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -10,10 +10,10 @@
 
 terraform {
   backend "s3" {
-    bucket          = "use1-sqa-devops-tfstate"
+    bucket          = "use1-${lower(local.local_data.tag_env)}-devops-tfstate"
     key             = "kiosk_infrastructure/terraform.tfstate"
-    region          = "us-east-1"
-    dynamodb_table  = "use1-sqa-devops-tfstate-lock"
+    region          = ${lower(local.local_data.aws_region)}
+    dynamodb_table  = "use1-${lower(local.local_data.tag_env)}-devops-tfstate-lock"
     encrypt         = true
   }
 }
