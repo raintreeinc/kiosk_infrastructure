e7df0bc2eaddfb4ce06b4fd19cb95027794bbc7e
Sushant Sonarghare <sushants@cybage.com>
2022-09-01 11:53:00 +0530

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -140,7 +140,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     }
   }
   tags = {
-    Environment = -"${lower(local.local_data.tag_env)}"
+    Environment = -"development"
     Name        = "${lower(local.local_data.tag_prefix)}-cloudfront-dstribution-${lower(local.local_data.tag_env)}-${lower(local.local_data.tag_project)}"
   }
   viewer_certificate {
