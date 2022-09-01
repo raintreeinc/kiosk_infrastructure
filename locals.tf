cb3ae9b7006dbe5ab2c95a0ef661bd01af9dc2c3
Sushant Sonarghare <sushants@cybage.com>
2022-09-01 11:15:35 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,3 +1,4 @@
 locals {
-  local_data   = jsondecode(file("${path.module}/variables.tfvars.json"))
+#  local_data   = jsondecode(file("${path.module}/variables.tfvars.json"))
+   local_data   = jsondecode(file("${path.module}/dev.tfvars.json"))
 }
