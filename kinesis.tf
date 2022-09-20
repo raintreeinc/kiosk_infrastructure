b1aaf7553c8bac5b6d9e040e3b119a8c9e2312cc
Sushant Sonarghare <sushants@cybage.com>
2022-09-20 17:41:40 +0530

    Update kinesis.tf

--- kinesis.tf
+++ kinesis.tf
@@ -11,3 +11,9 @@ resource "aws_dynamodb_kinesis_streaming_destination" "Patientstream" {
   stream_arn = aws_kinesis_stream.Kioskstream.arn
   table_name = "Patients"
 }
+
+resource "aws_lambda_event_source_mapping" "kiosk-sqs-lambdamapping-dev"{
+   event_source_arn = aws_kinesis_stream.Kioskstream.arn
+   function_name = "arn:aws:lambda:${lower(local.local_data.aws_region)}:${local.accountid}:function:${lower(local.local_data.tag_prefix)}-dynamodb-kinesis-stream-updates-${lower(local.local_data.tag_env)}-${lower(local.local_data.tag_project)}"
+   
+ }
