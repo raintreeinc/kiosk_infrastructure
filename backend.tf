e0e2c2808e40166e5d3a91cff2e3493322dd96a4
Sushant Sonarghare <sushants@cybage.com>
2022-09-21 18:30:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -13,7 +13,7 @@ terraform {
     bucket          = "use1-uat-infra-tfstate"
     key             = "kiosk_infrastructure/terraform.tfstate"
     region          = "us-east-1"
-    dynamodb_table  = "use1-uat-devops-tfstate-lock"
+    dynamodb_table  = "use1-uat-infra-tfstate-lock"
     encrypt         = true
   }
 }
