4b696dfa84f02c3e3637747a082fd575f631e41e
Sushant Sonarghare <sushants@cybage.com>
2022-09-27 18:27:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,34 +1,3 @@
 
 
 # s3 varibles #
-
-
-variable "s3-name" {
-  type = string
-#  default = "rt-s3-raintree-eventdata-dev-kiosk"
-  default = "rt-s3-raintree-eventdata-sqa-kiosk"
-  
-}
-
-variable "Environment" {
-  type = string
-#  default = "dev"
-  default = "sqa"
-}
-
-variable "status" {
-  type = string
-  default = "Enabled"
-  
-}
-
-
-#sqs queue
-
-variable "sqs-queue" {
-
-  type = string
-#  default = "rt-dailybatchdata-sqs-dev-kiosk"
-  default = "rt-dailybatchdata-sqs-sqa-kiosk"
-  
-}
