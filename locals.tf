e123e386f807e95b7d7c60f57653f4f9c863eeea
sivakrishnako <101097114+sivakrishnako@users.noreply.github.com>
2022-10-03 22:18:58 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
 #  local_data   = jsondecode(file("${path.module}/variables.tfvars.json"))
-   local_data   = jsondecode(file("${path.module}/sqa.tfvars.json"))
+   local_data   = jsondecode(file("${path.module}/dev.tfvars.json"))
 }
