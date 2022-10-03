86fd2b3d7d1d9140aee23e424fca2fddd8250f1d
sivakrishnako <101097114+sivakrishnako@users.noreply.github.com>
2022-10-03 22:18:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -10,15 +10,15 @@ terraform {
 }
 
 # #SQA ENVT
-terraform {
-  backend "s3" {
-    bucket          = "use1-sqa-devops-tfstate"
-    key             = "kiosk_infrastructure/terraform.tfstate"
-    region          = "us-east-1"
-    dynamodb_table  = "use1-sqa-devops-tfstate-lock"
-    encrypt         = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket          = "use1-sqa-devops-tfstate"
+#     key             = "kiosk_infrastructure/terraform.tfstate"
+#     region          = "us-east-1"
+#     dynamodb_table  = "use1-sqa-devops-tfstate-lock"
+#     encrypt         = true
+#   }
+# }
 
 #UAT ENVT
 #terraform {
