d2b2f49f6f7ecf28c78c82182bed8fc0ed1feef5
devendrakuv <100337351+devendrakuv@users.noreply.github.com>
2022-10-11 11:29:31 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -21,12 +21,12 @@
 #  }
 
 #UAT ENVT
- terraform {
-   backend "s3" {
-     bucket          = "use1-uat-infra-tfstate"
-     key             = "kiosk_infrastructure/terraform.tfstate"
-     region          = "us-east-1"
-     dynamodb_table  = "use1-uat-infra-tfstate-lock"
-     encrypt         = true
-   }
- }
+# terraform {
+#  backend "s3" {
+#     bucket          = "use1-uat-infra-tfstate"
+#     key             = "kiosk_infrastructure/terraform.tfstate"
+#     region          = "us-east-1"
+#     dynamodb_table  = "use1-uat-infra-tfstate-lock"
+#     encrypt         = true
+#   }
+# }
