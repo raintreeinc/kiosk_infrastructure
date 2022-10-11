43bfea0d130fa78f2d9eab1d01345c0f1015a0b6
devendrakuv <100337351+devendrakuv@users.noreply.github.com>
2022-10-11 11:29:56 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,13 +1,13 @@
 # DEV ENVT
-# terraform {
-# backend "s3" {
-# bucket          = "use1-dev-devops-tfstate"
-# key             = "kiosk_infrastructure/terraform.tfstate"
-# region          = "us-east-1"
-# dynamodb_table  = "use1-dev-devops-tfstate-lock"
-# encrypt         = true
-# }
-# }
+ terraform {
+ backend "s3" {
+ bucket          = "use1-dev-devops-tfstate"
+ key             = "kiosk_infrastructure/terraform.tfstate"
+ region          = "us-east-1"
+ dynamodb_table  = "use1-dev-devops-tfstate-lock"
+ encrypt         = true
+ }
+}
 
 # SQA ENVT
 # terraform {
