11c1a445d8e2e1073033d960a5f35f4aab211cdf
sivakrishnako <101097114+sivakrishnako@users.noreply.github.com>
2022-10-31 12:25:56 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,13 +1,13 @@
 # DEV ENVT
-#  terraform {
-#  backend "s3" {
-#  bucket          = "use1-dev-devops-tfstate"
-#  key             = "kiosk_infrastructure/terraform.tfstate"
-#  region          = "us-east-1"
-#  dynamodb_table  = "use1-dev-devops-tfstate-lock"
-#  encrypt         = true
-#  }
-# }
+ terraform {
+ backend "s3" {
+ bucket          = "use1-dev-devops-tfstate"
+ key             = "kiosk_infrastructure/terraform.tfstate"
+ region          = "us-east-1"
+ dynamodb_table  = "use1-dev-devops-tfstate-lock"
+ encrypt         = true
+ }
+}
 
 # SQA ENVT
 #  terraform {
@@ -21,12 +21,12 @@
 #   }
 
 #UAT ENVT
-terraform {
-backend "s3" {
-   bucket          = "use1-uat-infra-tfstate"
-   key             = "kiosk_infrastructure/terraform.tfstate"
-   region          = "us-east-1"
-   dynamodb_table  = "use1-uat-infra-tfstate-lock"
-   encrypt         = true
- }
-}
+# terraform {
+# backend "s3" {
+#    bucket          = "use1-uat-infra-tfstate"
+#    key             = "kiosk_infrastructure/terraform.tfstate"
+#    region          = "us-east-1"
+#    dynamodb_table  = "use1-uat-infra-tfstate-lock"
+#    encrypt         = true
+#  }
+# }
