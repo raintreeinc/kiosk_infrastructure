6a06cb9fe1c4e53b86a21e0bbfa79a812abb17cf
Sushant Sonarghare <sushants@cybage.com>
2022-11-25 12:07:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,24 +1,24 @@
 # DEV ENVT
-# terraform {
-# backend "s3" {
-# bucket          = "use1-dev-devops-tfstate"
-# key             = "kiosk_infrastructure/terraform.tfstate"
-# region          = "us-east-1"
-# dynamodb_table  = "use1-dev-devops-tfstate-lock"
-# encrypt         = true
-# }
-# }
+ terraform {
+ backend "s3" {
+ bucket          = "use1-dev-devops-tfstate"
+ key             = "kiosk_infrastructure/terraform.tfstate"
+ region          = "us-east-1"
+ dynamodb_table  = "use1-dev-devops-tfstate-lock"
+ encrypt         = true
+ }
+ }
 
 # SQA ENVT
-   terraform {
-     backend "s3" {
-        bucket          = "use1-sqa-devops-tfstate"
-       key             = "kiosk_infrastructure/terraform.tfstate"
-       region          = "us-east-1"
-       dynamodb_table  = "use1-sqa-devops-tfstate-lock"
-       encrypt         = true
-      }
-   }
+#   terraform {
+#     backend "s3" {
+#        bucket          = "use1-sqa-devops-tfstate"
+#       key             = "kiosk_infrastructure/terraform.tfstate"
+#       region          = "us-east-1"
+#       dynamodb_table  = "use1-sqa-devops-tfstate-lock"
+#       encrypt         = true
+#      }
+#   }
 
 #UAT ENVT
 #  terraform {
