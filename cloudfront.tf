02734735cfbcb38eb6b551e2b692d20742dec0d7
Sushant Sonarghare <sushants@cybage.com>
2022-12-12 16:13:49 +0530

    Update cloudfront.tf
    Updated Cloudfront geographic restriction to none

--- cloudfront.tf
+++ cloudfront.tf
@@ -135,8 +135,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   price_class = "PriceClass_200"
   restrictions {
     geo_restriction {
-      restriction_type = "whitelist"
-      locations        = ["US", "CA", "GB", "DE", "IN"]
+      restriction_type = "none"
+      locations        = []
     }
   }
   tags = {
